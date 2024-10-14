3b09bab3610c28fd1584dabcad8827135ebfa5ab
GouravSaini2004 <mrgoravsainimrt@gmail.com>
2024-10-14 12:47:34 +0530

    login

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -28,7 +28,7 @@ exports.register = async (req, res) => {
         );
         const verificationLink = `https://gamingbackend-dkf6.onrender.com/user/verify_email?token=${jwttoken}`
         const mailOptions = {
-            from: "gamekarao@gmail.com",
+            from: "mrgoravsainimrt@gmail.com",
             to: email,
             subject: 'Email Verification',
             text: `Please verify your email by clicking on the following link: ${verificationLink}`,
